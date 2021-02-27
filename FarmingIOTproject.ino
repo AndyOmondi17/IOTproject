ecc5e2145a88de508b291bb4fbb129b40d887508
AndyOmondi17 <andyomondi17@gmail.com>
2021-02-27 19:20:46 +0300

    add motion sensor functionality

--- FarmingIOTproject.ino
+++ FarmingIOTproject.ino
@@ -8,10 +8,33 @@ DHT dht(DHTPIN, DHTTYPE);
 const int dry = 578;
 const int wet = 298;
 
+int calibrationTime = 30; 
+//the time when the sensor outputs a low impulse long unsigned int lowIn; 
+//the amount of milliseconds the sensor has to be low 
+//before we assume all motion has stopped
+long unsigned int lowIn;
+long unsigned int pause = 5000; 
+boolean lockLow = true; 
+boolean takeLowTime; 
+int pirPin = 3;  
+int ledPin = 13;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println(F("DHTxx test!"));
+//  Serial.println(F("Sensors Loading"));
+  pinMode(pirPin, INPUT); 
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(pirPin, LOW); //give the sensor some time to calibrate 
+  Serial.print("calibrating sensors ");
+//  will load for 30 seconds before ending
+  for(int i = 0; i < calibrationTime; i++)
+  { Serial.print("."); 
+    delay(1000); 
+  } 
+  Serial.println(" done"); 
+  Serial.println("Sensors are active"); 
+  delay(50); 
 
   dht.begin();
 }
@@ -65,4 +88,33 @@ void loop(){
   Serial.print(F("°C "));
 //  Serial.print(hif);
   Serial.println(F(""));
-}
+
+  if(digitalRead(pirPin) == HIGH)
+    { digitalWrite(ledPin, HIGH); //the led visualizes the sensors output pin state 
+        if(lockLow){ //makes sure we wait for a transition to LOW before any further output is made: 
+            lockLow = false; 
+            Serial.println("---"); 
+            Serial.print("motion detected at "); 
+            Serial.print(millis()/1000); 
+            Serial.println(" sec"); 
+            delay(50); 
+            } takeLowTime = true; 
+            } 
+            if(digitalRead(pirPin) == LOW)
+              { digitalWrite(ledPin, LOW); //the led visualizes the sensors output pin state 
+                if(takeLowTime)
+                { lowIn = millis(); //save the time of the transition from high to LOW 
+                    takeLowTime = false; 
+                    //make sure this is only done at the start of a LOW phase 
+                } //if the sensor is low for more than the given pause, //we assume that no more motion is going to happen 
+                if(!lockLow && millis() - lowIn > pause)
+                    { //makes sure this block of code is only executed again after //a new motion sequence has been detected 
+                      lockLow = true; 
+                      Serial.print("motion ended at "); //output 
+                      Serial.print((millis() - pause)/1000); 
+                      Serial.println(" sec"); 
+                      delay(50); 
+                    } 
+                }
+                 delay(2000);
+              }
